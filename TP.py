02651df9421f317beeb2bfbb9d1f69b2437fb73e
ELgagadhkt <61547651+ELgagadhkt@users.noreply.github.com>
2022-05-18 10:17:29 +0200

    Update TP.py

--- TP.py
+++ TP.py
@@ -2,40 +2,33 @@ import random
 import time
 
 def EuclideEtendu(a, b):
-    if (b==0):
-        return(a,1,0)
+    if b == 0:
+        return a, 1, 0
 
-    res = EuclideEtendu(b, a%b)
+    res = EuclideEtendu(b, a % b)
 
     dp = res[0]
     xp = res[1]
     yp = res[2]
 
-    return (dp,yp,xp - a/byp)
+    return dp, yp, xp - a / b * yp
 
-def ExpMod(a,k,n):
+
+def ExpMod(a, k, n):
     p = 1
     while k > 0:
-        if k%2 != 0:
-            p = (p a)%n
-        a = (a*a)%n
-        k = k//2
+        if k % 2 != 0:
+            p = (p * a) % n
+        a = (a * a) % n
+        k = k // 2
     return p
 
-def TestFernat(n):
-    return ExpMod(2, n - 1, n) == 1
-
 def bin(n):
-    q = -1
-    res = ''
-    while q != 0:
-        q = n // 2
-        r = n % 2
-        res = r + res
-        n = q
-    return res
+    if n >= 1:
+        bin(n // 2)
+    print(n % 2, end='')
 
-def TestPrimaliteNaif(n):
+#def TestPrimaliteNaif(n):
     #transformer un entier en serie de bits, mettre à un les premiers, ce qui sera légèrement au dessus de la racine de ce dernier
 
-print(ExpMod(2,6,7))
+print(bin(10))
