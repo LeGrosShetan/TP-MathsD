71d649d23cb6a07dcd86b37663491325a2927141
LeGrosShetan <maxagaz2002@hotmail.fr>
2022-05-18 09:01:25 +0200

    Ex1 + 2

---
+++ TP.py
@@ -0,0 +1,27 @@
+def EuclideEtendu(a, b):
+    if (b==0):
+        return(a,1,0)
+    
+    res = EuclideEtendu(b, a%b)
+    
+    dp = res[0]
+    xp = res[1]
+    yp = res[2]
+    
+    return (dp,yp,xp - a/b*yp)
+
+def ExpMod(a,k,n):
+    p=1
+    
+    while k>0:
+        if k%2!=0:
+            p = (p*a)%n
+            
+        a = (a*a)%n
+        k = k/2
+        
+    return p
+
+
+
+print(EuclideEtendu(3,6))
