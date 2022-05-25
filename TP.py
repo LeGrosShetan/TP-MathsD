a21cb118374a23d42662729a5062cb2a7446f4c1
ELgagadhkt <61547651+ELgagadhkt@users.noreply.github.com>
2022-05-25 09:27:31 +0200

    sqrt

--- TP.py
+++ TP.py
@@ -23,12 +23,33 @@ def ExpMod(a, k, n):
         k = k // 2
     return p
 
-def bin(n):
-    if n >= 1:
-        bin(n // 2)
-    print(n % 2, end='')
+def TestFernat(n):
+    return ExpMod(2, n - 1, n) == 1
+
+def Square(n,i,j):
+    mid=(i+j)/2
+    mul=mid*mid
+    if mul==n or abs(mul-n)<0.00001:
+        return mid
+    elif mul<n:
+        return Square(n,mid,j)
+    else:
+        return Square(n,i,mid)
+
+def sqrt(n):
+    i=1
+    found = False
+    while not found:
+        if i*i >n:
+            found = True
+            return i
+        elif i*i>n:
+            res = Square(n,i-1,i)
+            found = True
+            return res
+        i=i+1
 
 #def TestPrimaliteNaif(n):
     #transformer un entier en serie de bits, mettre à un les premiers, ce qui sera légèrement au dessus de la racine de ce dernier
 
-print(bin(10))
+print(sqrt(100))
